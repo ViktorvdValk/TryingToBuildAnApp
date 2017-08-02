23a79914c91ad093a915c0c3d537b5b49f5b775a
Viktor Van der Valk <viktorvandervalk@viktors-mbp.home>
2017-08-02 12:22:41 +0200

    Initial Commit

---
+++ SymptomResearchApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SymptomResearchApp
+//
+//  Created by Viktor Van der Valk on 02/08/2017.
+//  Copyright © 2017 Viktor Van der Valk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
